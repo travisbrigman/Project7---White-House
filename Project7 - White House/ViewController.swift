2f4c1f61c8c0cb7e197e8260a14289c56766e855
travisbrigman <travis1000@gmail.com>
2021-05-13 15:59:41 -0500

    Initial Commit

---
+++ Project7 - White House/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project7 - White House
+//
+//  Created by Travis Brigman on 5/13/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
